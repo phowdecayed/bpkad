10c1a69f13b2e54f6ab4c1536933eb1b63263e00
phowdecayed <kafrabot@gmail.com>
2021-01-18 12:40:25 +0700

    first commit

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,68 @@
+<!--Slider Start-->
+         <section id="home-slider" class="owl-carousel owl-theme wf100">
+            <?php foreach($slider as $slider) { ?>
+            <div class="item">
+               <div class="slider-caption h3slider">
+                  <div class="container">
+                     <?php if($slider->status_text=="Ya") { ?>
+                     <strong>{{ strip_tags($slider->isi) }}</strong>
+                     <h1>{{ $slider->judul_galeri }}</h1>
+                     <a href="{{ $slider->website }}">Baca detail</a>
+                     <?php } ?>
+                  </div>
+               </div>
+               <img src="{{ asset('assets/upload/image/'.$slider->gambar) }}" alt="">
+            </div>
+            <?php } ?>
+         </section>
+         <!--Slider End-->
+         <!--Service Area Start-->
+         <!--Service Area End-->
+            <!--About Txt Video End-->
+            <!--About Section Start-->
+         <!--About Section End-->
+
+         <!--Blog Start-->
+<section class="h2-news wf100 p80 blog">
+   <div class="blog-grid">
+      <div class="container">
+         <div class="row">
+            <div class="col-md-6">
+               <div class="section-title-2">
+                  <h5>Baca update kami</h5>
+                  <h2>Berita & Updates</h2>
+               </div>
+            </div>
+            <div class="col-md-6"> <a href="{{ asset('berita') }}" class="view-more">Lihat berita lainnya</a> </div>
+            <div class="col-md-12">
+               <hr>
+            </div>
+         </div>
+         <div class="row" style="background-color: white; padding-top: 20px; padding-bottom: 20px; border-radius: 5px;">
+            <?php foreach($berita as $berita) { ?>
+            <!--Blog Small Post Start-->
+            <div class="col-md-4 col-sm-6" >
+               <div class="blog-post">
+                  <div class="blog-thumb"> <a href="{{ asset('berita/read/'.$berita->slug_berita) }}"><i class="fas fa-link"></i></a> <img src="{{ asset('assets/upload/image/thumbs/'.$berita->gambar) }}" alt="><?php  echo $berita->judul_berita ?>"> </div>
+                  <div class="post-txt">
+                     <h5><a href="{{ asset('berita/read/'.$berita->slug_berita) }}"><?php  echo $berita->judul_berita ?></a></h5>
+                     <ul class="post-meta">
+                        <li> <a href="{{ asset('berita/read/'.$berita->slug_berita) }}"><i class="fas fa-calendar-alt"></i> {{ tanggal('tanggal_id',$berita->tanggal_post)}}</a> </li>
+                        <li> <a href="{{ asset('berita/kategori/'.$berita->slug_berita) }}"><i class="fas fa-sitemap"></i> {{ $berita->nama_kategori }}</a> </li>
+                     </ul>
+                     <p><?php echo \Illuminate\Support\Str::limit(strip_tags($berita->isi), 100, $end='...') ?></p>
+                     <a href="{{ asset('berita/read/'.$berita->slug_berita) }}" class="read-post">Baca detail</a>
+                  </div>
+               </div>
+            </div>
+            <!--Blog Small Post End-->
+            <?php } ?>
+         </div>
+
+      </div>
+   </div>
+</section>
+<!--Blog End-->
+
+<!--Testimonials Start-->
+<!--Testimonials End-->
