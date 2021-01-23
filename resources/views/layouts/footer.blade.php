53f9d46d7096cb2691d0dbecfe4debe075262a73
phowdecayed <kafrabot@gmail.com>
2021-01-23 23:35:35 +0700

    edit

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -87,12 +87,12 @@ $nav_layananf = $myprofil->nav_layanan();
 <div id="gotoTop" class="icon-angle-up"></div>
 <!-- External JavaScripts
 	============================================= -->
-<script src="{{ asset('assets/js/jquery.js') }}"></script>
-<script src="{{ asset('assets/js/plugins.js') }}"></script>
+<script src="{{ asset('/assets/js/jquery.js') }}"></script>
+<script src="{{ asset('/assets/js/plugins.js') }}"></script>
 
 <!-- Footer Scripts
     ============================================= -->
-<script src="{{ asset('assets/js/functions.js') }}"></script>
+<script src="{{ asset('/assets/js/functions.js') }}"></script>
 
 </body>
 
--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -16,15 +16,15 @@ $site_config = DB::table('konfigurasi')->first();
     <!-- Stylesheets
 	============================================= -->
     <link href="https://fonts.googleapis.com/css?family=Lato:300,400,400i,700|Poppins:300,400,500,600,700|PT+Serif:400,400i&display=swap" rel="stylesheet" type="text/css" />
-    <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.css') }}" type="text/css" />
-    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}" type="text/css" />
-    <link rel="stylesheet" href="{{ asset('assets/css/swiper.css') }}" type="text/css" />
-    <link rel="stylesheet" href="{{ asset('assets/css/dark.css') }}" type="text/css" />
-    <link rel="stylesheet" href="{{ asset('assets/css/font-icons.css') }}" type="text/css" />
-    <link rel="stylesheet" href="{{ asset('assets/css/animate.css') }}" type="text/css" />
-    <link rel="stylesheet" href="{{ asset('assets/css/magnific-popup.css') }}" type="text/css" />
+    <link rel="stylesheet" href="{{ asset('/assets/css/bootstrap.css') }}" type="text/css" />
+    <link rel="stylesheet" href="{{ asset('/assets/css/style.css') }}" type="text/css" />
+    <link rel="stylesheet" href="{{ asset('/assets/css/swiper.css') }}" type="text/css" />
+    <link rel="stylesheet" href="{{ asset('/assets/css/dark.css') }}" type="text/css" />
+    <link rel="stylesheet" href="{{ asset('/assets/css/font-icons.css') }}" type="text/css" />
+    <link rel="stylesheet" href="{{ asset('/assets/css/animate.css') }}" type="text/css" />
+    <link rel="stylesheet" href="{{ asset('/assets/css/magnific-popup.css') }}" type="text/css" />
 
-    <link rel="stylesheet" href="{{ asset('assets/css/custom.css') }}" type="text/css" />
+    <link rel="stylesheet" href="{{ asset('/assets/css/custom.css') }}" type="text/css" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <?php echo $site_config->metatext ?>
     <!-- Document Title
